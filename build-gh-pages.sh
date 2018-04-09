2724a98d2ec29ccc65e3eb6dc36dceeac5663677
Varun Patil <radialapps@gmail.com>
2018-04-09 20:11:09 +0530

    FiX

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -4,7 +4,7 @@ mkdir built
 gem install jekyll
 JEKYLL_ENV=production jekyll build --destination built --config _config.gh-pages.yml
 
-if [ "$TRAVIS_PULL_REQUEST" != "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 
 cd built && tar -zcvf ../package.tar.gz . && cd -
 curl -H "PR: $TRAVIS_PULL_REQUEST" --upload-file package.tar.gz -X POST https://temp-iitb.radialapps.com/webhook/
